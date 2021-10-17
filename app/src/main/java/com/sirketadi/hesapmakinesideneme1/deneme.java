2cd32be79ac14847e67054e0fba361ea2c32ff64
mrtSahin <mrtsvs07@gmail.com>
2021-10-17 12:34:26 +0300

    OnlukSistem den İkilkSistem e çeviren uygulama.

---
+++ app/src/main/java/com/sirketadi/hesapmakinesideneme1/deneme.java
@@ -0,0 +1,14 @@
+package com.sirketadi.hesapmakinesideneme1;
+
+public class deneme {
+    private String isim;
+    private int no;
+
+    public deneme() {
+    }
+
+    public deneme(String isim, int no) {
+        this.isim = isim;
+        this.no = no;
+    }
+}
