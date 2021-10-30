47820c718e3d671a1338c049d1f5c2999d3fde4b
mrtSahin <mrtsvs07@gmail.com>
2021-10-30 12:10:16 +0300

    OnlukSistem den İkilkSistem e çeviren uygulama.

--- app/src/main/java/com/sirketadi/hesapmakinesideneme1/MainActivity.java
+++ app/src/main/java/com/sirketadi/hesapmakinesideneme1/MainActivity.java
@@ -1,13 +1,20 @@
 package com.sirketadi.hesapmakinesideneme1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.GridView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.sirketadi.hesapmakinesideneme1.databinding.ActivityMainBinding;
@@ -17,13 +24,15 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding views;
     private Cevir cevir;
+    private Menu menu;
 
-    private ArrayList<String> sayilar =new ArrayList<>();
+    private ArrayList<String> sayilar = new ArrayList<>();
     private ArrayAdapter<String> veriAdaptoru;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        views= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        views = DataBindingUtil.setContentView(this, R.layout.activity_main);
         sayilar.add("1");
         sayilar.add("2");
         sayilar.add("3");
@@ -36,11 +45,15 @@ public class MainActivity extends AppCompatActivity {
         sayilar.add("Sil");
         sayilar.add("0");
 
+        views.toolbar.setTitle("HesapMakinesi");
+        views.toolbar.setTitleTextColor(getColor(R.color.yaziRengi));
+        setSupportActionBar(views.toolbar);
 
-        cevir=new Cevir();
+
+        cevir = new Cevir();
 
 
-        veriAdaptoru=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1,sayilar);
+        veriAdaptoru = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, sayilar);
         views.gridView.setAdapter(veriAdaptoru);
 
         views.gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -48,13 +61,13 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
 
-                if(sayilar.get(i)=="Sil"&&(views.textViewGiris.getText().length()-1)>=0){
-                    views.textViewGiris.setText(String.valueOf(views.textViewGiris.getText()).substring(0,views.textViewGiris.getText().length()-1));
-                }else if(sayilar.get(i)=="Sil"){
-                    Toast.makeText(getApplicationContext(),"SayiDeğeriGiriniz",Toast.LENGTH_SHORT).show();
+                if (sayilar.get(i) == "Sil" && (views.textViewGiris.getText().length() - 1) >= 0) {
+                    views.textViewGiris.setText(String.valueOf(views.textViewGiris.getText()).substring(0, views.textViewGiris.getText().length() - 1));
+                } else if (sayilar.get(i) == "Sil") {
+                    Toast.makeText(getApplicationContext(), "SayiDeğeriGiriniz", Toast.LENGTH_SHORT).show();
 
-                }else{
-                    views.textViewGiris.setText(views.textViewGiris.getText()+sayilar.get(i));
+                } else {
+                    views.textViewGiris.setText(views.textViewGiris.getText() + sayilar.get(i));
                 }
             }
         });
@@ -64,15 +77,37 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 views.textViewSonuc.setText("");
 
-                if(views.textViewGiris.getText().length()>0) {
+                if (views.textViewGiris.getText().length() > 0) {
                     String sonuc = cevir.hesapla(Integer.parseInt(String.valueOf(views.textViewGiris.getText())));
                     views.textViewSonuc.setText(sonuc);
+                } else {
+                    Toast.makeText(getApplicationContext(), "DeğerGiriniz", Toast.LENGTH_SHORT).show();
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"DeğerGiriniz",Toast.LENGTH_SHORT).show();
-                }
-                }
+            }
 
         });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mode_button:
+                if (getDelegate().getLocalNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    return true;
+                } else {
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    return true;
+                }
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
